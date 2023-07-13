29ba18957556d8e60a870ba517c5cd771f8c1c91
Radan1t <safonchyk@ukr.net>
2023-07-13 23:58:09 +0300

    Створюємо меню де відбувається логін та регістрація

--- login.py
+++ login.py
@@ -1,3 +1,6 @@
+import tkinter as tk
+from tkinter import messagebox
+
 class Regist:
     def __init__(self):
         self.name = ""
@@ -7,9 +10,83 @@ class Regist:
         self.phone_number = ""
         self.password = ""
 
+def show_registration_window(parent_window):
+    registration_window = tk.Toplevel(parent_window)
+    registration_window.title("Registration")
+    registration_window.geometry("300x300")
+
+    name_label = tk.Label(registration_window, text="Name:")
+    name_label.pack()
+
+    name_entry = tk.Entry(registration_window)
+    name_entry.pack()
+
+    surname_label = tk.Label(registration_window, text="Surname:")
+    surname_label.pack()
+
+    surname_entry = tk.Entry(registration_window)
+    surname_entry.pack()
+
+    email_label = tk.Label(registration_window, text="Email:")
+    email_label.pack()
+
+    email_entry = tk.Entry(registration_window)
+    email_entry.pack()
+
+    phone_label = tk.Label(registration_window, text="Phone Number:")
+    phone_label.pack()
+
+    phone_entry = tk.Entry(registration_window)
+    phone_entry.pack()
+
+    login_label = tk.Label(registration_window, text="Login:")
+    login_label.pack()
+
+    login_entry = tk.Entry(registration_window)
+    login_entry.pack()
+
+    password_label = tk.Label(registration_window, text="Password:")
+    password_label.pack()
+
+    password_entry = tk.Entry(registration_window, show="*")
+    password_entry.pack()
+
+    password_test_label = tk.Label(registration_window, text="Confirm Password:")
+    password_test_label.pack()
+
+    password_test_entry = tk.Entry(registration_window, show="*")
+    password_test_entry.pack()
+
+    register_button = tk.Button(registration_window, text="Register", command=lambda: register(user, name_entry, surname_entry, email_entry, phone_entry, login_entry, password_entry, password_test_entry, registration_window))
+    register_button.pack()
+
+def register(user, name_entry, surname_entry, email_entry, phone_entry, login_entry, password_entry, password_test_entry, registration_window):
+    user.name = name_entry.get()
+    user.surname = surname_entry.get()
+    user.email = email_entry.get()
+    user.phone_number = phone_entry.get()
+    user.login = login_entry.get()
+
+    user.password = password_entry.get()
+    password_test = password_test_entry.get()
+
+    if user.password == password_test:
+        with open("register.txt", "a") as file:
+            file.write("Login: " + user.login + '\n')
+            file.write("Name: " + user.name + '\n')
+            file.write("Surname: " + user.surname + '\n')
+            file.write("Email: " + user.email + '\n')
+            file.write("Phone Number: " + user.phone_number + '\n')
+            file.write("Password: " + user.password + '\n')
+
+        messagebox.showinfo("Registration Successful", "Registration successful!")
+        registration_window.destroy()
+    else:
+        messagebox.showerror("Password Mismatch", "Passwords do not match. Please try again.")
+
 def login():
-    login = input("Enter your login: ")
-    password = input("Enter your password: ")
+    login = login_entry.get()
+    password = password_entry.get()
 
     with open("register.txt", "r") as file:
         for line in file:
@@ -21,65 +98,37 @@ def login():
                 user.email = file.readline().split("Email:")[1].strip()
                 user.phone_number = file.readline().split("Phone Number:")[1].strip()
                 user.password = file.readline().split("Password:")[1].strip()
+
                 if user.login == login and user.password == password:
-                    print("Login successful!")
-                    print("Name:", user.name)
-                    print("Surname:", user.surname)
-                    print("Email:", user.email)
-                    print("Phone Number:", user.phone_number)
+                    messagebox.showinfo("Login Successful", f"Name: {user.name}\nSurname: {user.surname}\nEmail: {user.email}\nPhone Number: {user.phone_number}")
                     return
 
-        print("Login failed. Invalid login or password.")
+        messagebox.showerror("Login Failed", "Invalid login or password.")
 
-def register():
+def main():
+    global login_entry, password_entry, user
+    window = tk.Tk()
+    window.title("Login/Register")
+    window.geometry("300x300")
+
+    choice_label = tk.Label(window, text="Select an option:")
+    choice_label.pack()
+
+    login_entry = tk.Entry(window)  # Added login entry field
+    login_entry.pack()
+
+    password_entry = tk.Entry(window, show="*")  # Added password entry field
+    password_entry.pack()
+
+    login_button = tk.Button(window, text="Login", command=login)
+    login_button.pack()
+
+    registration_button = tk.Button(window, text="Registration", command=lambda: show_registration_window(window))
+    registration_button.pack()
+
     user = Regist()
-    try:
-        with open("register.txt", "a") as file:
-            user.name = input("Enter your first name: ")
-            user.surname = input("Enter your last name: ")
-            user.login = input("Enter your login: ")
-            user.email = input("Enter your email: ")
-            user.phone_number = input("Enter your phone number: ")
 
-            # Loop until passwords match
-            while True:
-                user.password = input("Enter your password: ")
-                password_test = input("Enter your password again: ")
-
-                if user.password == password_test:
-                    break  # Passwords match, exit the loop
-                else:
-                    print("Passwords do not match. Please try again.")
-
-            # Save user data to file
-            file.write("Login: " + user.login + '\n')
-            file.write("Name: " + user.name + '\n')
-            file.write("Surname: " + user.surname + '\n')
-            file.write("Email: " + user.email + '\n')
-            file.write("Phone Number: " + user.phone_number + '\n')
-            file.write("Password: " + user.password + '\n')
-
-        print("Registration successful!")
-    except IOError:
-        print("Error opening file!")
-        exit(1)
-
-def main():   
-    while True:
-        print("Enter 1 to log in")
-        print("Enter 2 to register")
-        print("Enter 3 to exit")
-        choice = input()
-
-        if choice == "1":
-            login()
-            break
-        elif choice == "2":
-            register()
-        elif choice == "3":
-            break
-        else:
-            print("Invalid choice. Please try again.")
+    window.mainloop()
 
 if __name__ == "__main__":
     main()
